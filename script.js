0a1532f92eec846a19a4ffbe6285b49d82c1dc53
egnica <46502133+egnica@users.noreply.github.com>
2024-12-30 08:22:07 -0600

    commit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const element = document.createElement("span");
 element.innerText = "Hello world";
 document.body.appendChild(element);
+
+document.getElementsById("div-id");
+document.getElementsByClassName("div-class");
