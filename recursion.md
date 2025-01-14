acdc555ab8c01102e0040d574f17ec43b95b7f97
egnica <46502133+egnica@users.noreply.github.com>
2025-01-14 10:20:10 -0600

    commit

---
+++ recursion.md
@@ -0,0 +1,9 @@
+# What is Recursion?
+
+A function that calls it's self until it reaches some stopping condition
+
+Any situation where you do something, and depending on the results, you might do it again
+
+## Has 2 parts
+1. Base Case - stopping condition 
+2. The recursive call to that function
--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-console.log("test");
-// Create a variable that contains an array.
-const thisArray = ["one", "two", "three", "four", "five"];
-// Using an if statement with else if I want you to check the length of the array
+// What is recursion? A function that calls it's self
 
-// If it is empty print out "empty"
-// If it has less than 5 elements print out "small"
-// If it has less than 10 elements print out "medium"
-// Otherwise print out "large"
-const arrayLength = thisArray.length;
 
-arrayLength == ""
-  ? console.log("empty")
-  : arrayLength < 5
-  ? console.log("small")
-  : arrayLength < 10
-  ? console.log("medium")
-  : console.log("large");
-
-for (let x = 0; x < 11; x++) {
-  if (x == 5) break;
-  console.log(`x:${x}`);
-}
-let i = 0;
-while (i < 10) {
-  console.log(`i:${i}`);
-  i++;
-}
